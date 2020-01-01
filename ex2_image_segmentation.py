4d03dc53c5a944441454177674e9a5ef092b7ab6
ZhaoQing <zhaoqing9797@qq.com>
2020-01-01 12:23:12 +0800

    add canny

--- ex2_image_segmentation.py
+++ ex2_image_segmentation.py
@@ -141,24 +141,149 @@ def image_get_label(img, mode="THRESHOLD_OSTU"):
 
     elif mode == "EDGE_BASED":
         # Gaussian smoothing
-        smooth_img = space_filter(img, fil_type="GRAY",
-                                  fil=filters.gaussian_fil_5x5, mode="SAME")
-        plt.imsave("smooth_img.jpg", smooth_img)
+        img = space_filter(img, fil_type="GRAY",
+                           fil=filters.gaussian_fil_5x5, mode="SAME")
 
-        # get the edge
-        edge_img = space_filter(smooth_img, fil_type="GRAY",
-                                fil=filters.laplacian_fil_8, mode="SAME")
+        # get direction matrix
+        copy_img = np.zeros(img.shape, dtype="uint8")
+        theta = np.zeros(img.shape, dtype="float")  # direction matrix
+        img = cv2.copyMakeBorder(
+            img, 1, 1, 1, 1, borderType=cv2.BORDER_REPLICATE)
+        m1 = filters.prewitt_fil_x
+        m2 = filters.prewitt_fil_y
+        rows, cols = img.shape
+        for i in range(1, rows-1):
+            for j in range(1, cols-1):
+                Gy = (np.dot(np.array(
+                    [1, 1, 1]), (m1 * img[i - 1:i + 2, j - 1:j + 2]))).dot(np.array([[1], [1], [1]]))
+                Gx = (np.dot(np.array(
+                    [1, 1, 1]), (m2 * img[i - 1:i + 2, j - 1:j + 2]))).dot(np.array([[1], [1], [1]]))
+                if Gx[0] == 0:
+                    theta[i-1, j-1] = 90
+                    continue
+                else:
+                    temp = (np.arctan(Gy[0] / Gx[0])) * 180 / np.pi
+                if Gx[0]*Gy[0] > 0:
+                    if Gx[0] > 0:
+                        theta[i-1, j-1] = np.abs(temp)
+                    else:
+                        theta[i-1, j-1] = (np.abs(temp) - 180)
+                if Gx[0] * Gy[0] < 0:
+                    if Gx[0] > 0:
+                        theta[i-1, j-1] = (-1) * np.abs(temp)
+                    else:
+                        theta[i-1, j-1] = 180 - np.abs(temp)
+                copy_img[i-1, j-1] = (np.sqrt(Gx**2 + Gy**2))
+        plt.imsave("copy_img.jpg", copy_img)
+        for i in range(1, rows - 2):
+            for j in range(1, cols - 2):
+                if (((theta[i, j] >= -22.5) and (theta[i, j] < 22.5)) or
+                        ((theta[i, j] <= -157.5) and (theta[i, j] >= -180)) or
+                        ((theta[i, j] >= 157.5) and (theta[i, j] < 180))):
+                    theta[i, j] = 0.0
+                elif (((theta[i, j] >= 22.5) and (theta[i, j] < 67.5)) or
+                      ((theta[i, j] <= -112.5) and (theta[i, j] >= -157.5))):
+                    theta[i, j] = 45.0
+                elif (((theta[i, j] >= 67.5) and (theta[i, j] < 112.5)) or
+                      ((theta[i, j] <= -67.5) and (theta[i, j] >= -112.5))):
+                    theta[i, j] = 90.0
+                elif (((theta[i, j] >= 112.5) and (theta[i, j] < 157.5)) or
+                      ((theta[i, j] <= -22.5) and (theta[i, j] >= -67.5))):
+                    theta[i, j] = -45.0
+        
+        # Non-Maximum Suppression(NMS)
+        nms_img = np.zeros(copy_img.shape)
+
+        for i in range(1, nms_img.shape[0]-1):
+            for j in range(1, nms_img.shape[1]-1):
+                if (theta[i, j] == 0.0) and (copy_img[i, j] == np.max([copy_img[i, j], copy_img[i+1, j], copy_img[i-1, j]])):
+                    nms_img[i, j] = copy_img[i, j]
+
+                if (theta[i, j] == -45.0) and copy_img[i, j] == np.max([copy_img[i, j], copy_img[i-1, j-1], copy_img[i+1, j+1]]):
+                    nms_img[i, j] = copy_img[i, j]
+
+                if (theta[i, j] == 90.0) and copy_img[i, j] == np.max([copy_img[i, j], copy_img[i, j+1], copy_img[i, j-1]]):
+                    nms_img[i, j] = copy_img[i, j]
+
+                if (theta[i, j] == 45.0) and copy_img[i, j] == np.max([copy_img[i, j], copy_img[i-1, j+1], copy_img[i+1, j-1]]):
+                    nms_img[i, j] = copy_img[i, j]
+        
+        plt.imsave("nms_img.jpg", nms_img)
+
+        # dual-threshold edge detection
+        edge_img = np.zeros(nms_img.shape)
+
+        TL = 0.2*np.max(nms_img)
+        TH = 0.3*np.max(nms_img)
+        
+        for i in range(1, edge_img.shape[0]-1):
+            for j in range(1, edge_img.shape[1]-1):
+                if nms_img[i, j] < TL:
+                    edge_img[i, j] = 0
+                    label_img[i, j] = 0
+                elif nms_img[i, j] > TH:
+                    edge_img[i, j] = 255
+                    label_img[i, j] = 1
+                elif ((nms_img[i+1, j] < TH) or (nms_img[i-1, j] < TH)or(nms_img[i, j+1] < TH)or
+                        (nms_img[i, j-1] < TH) or (nms_img[i-1, j-1] < TH)or (nms_img[i-1, j+1] < TH) or
+                        (nms_img[i+1, j+1] < TH) or (nms_img[i+1, j-1] < TH)):
+                    edge_img[i, j] = 255
+                    label_img[i, j] = 1
+
+        # edge_img = cv2.Canny(img,TL,TH) # use opencv
         plt.imsave("edge_img.jpg", edge_img)
+        return label_img
 
+    elif mode == "REGION_GROWING":
+        pass
+        seed_img = np.zeros((img_h, img_w), dtype="uint16")
+
+        for i in range(img_h-2):
+            for j in range(img_w-2):
+                gray_value = edge_img[i+1][j+1]
+                if gray_value >= 10:
+                    seed_img[i+1][j+1] = 0
+                else:
+                    seed_img[i+1][j+1] = 1
+
+        label = 1
+        label_img[1][1] = label
         for i in range(img_h):
             for j in range(img_w):
-                gray_value = edge_img[i][j]
-                if gray_value >= 10:
-                    label_img[i][j] = 0
-                else:
-                    label_img[i][j] = 1
-
-    return label_img
+                if seed_img[i][j] == 1:
+                    if label_img[i][j] == 0:
+                        if seed_img[i-1][j-1] == 1 and label_img[i-1][j-1] != 0:
+                            label_img[i][j] = label_img[i-1][j-1]
+                        elif seed_img[i-1][j] == 1 and label_img[i-1][j] != 0:
+                            label_img[i][j] = label_img[i-1][j]
+                        elif seed_img[i-1][j+1] == 1 and label_img[i-1][j+1] != 0:
+                            label_img[i][j] = label_img[i-1][j]
+                        elif seed_img[i][j-1] == 1 and label_img[i][j-1] != 0:
+                            label_img[i][j] = label_img[i][j-1]
+                        elif seed_img[i][j+1] == 1 and label_img[i][j+1] != 0:
+                            label_img[i][j] = label_img[i][j+1]
+                        elif seed_img[i+1][j-1] == 1 and label_img[i+1][j-1] != 0:
+                            label_img[i][j] = label_img[i+1][j-1]
+                        elif seed_img[i+1][j] == 1 and label_img[i+1][j] != 0:
+                            label_img[i][j] = label_img[i+1][j]
+                        elif seed_img[i+1][j+1] == 1 and label_img[i+1][j+1] != 0:
+                            label_img[i][j] = label_img[i+1][j+1]
+                        else:
+                            label += 1
+                            label_img[i][j] = label
+                if label > 6:
+                    break
+                img = img.astype(np.int32)
+                if (abs(img[i-1][j]-img[i][j])<10) and label_img[i-1][j] != 0:
+                    label_img[i][j] = label_img[i-1][j]
+                
+                elif (abs(img[i][j-1]-img[i][j])<10) and label_img[i][j-1] != 0:
+                    label_img[i][j] = label_img[i][j-1]
+                elif (abs(img[i][j+1]-img[i][j])<10) and label_img[i][j+1] != 0:
+                    label_img[i][j] = label_img[i][j+1]
+                
+                elif (abs(img[i+1][j]-img[i][j])<10) and label_img[i+1][j] != 0:
+                    label_img[i][j] = label_img[i+1][j]
 
 
 # fill each class of the image with different colors
@@ -188,7 +313,7 @@ def image_color_fill(label_img):
     return visual_img
 
 
-img = cv2.imread("org1.jpg")   # read the image
+img = cv2.imread("test3.jpg")   # read the image
 # change the image from BGR to RGB
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -200,7 +325,7 @@ print(img_type)
 plt.imshow(img, cmap="gray")  # show the image-=
 plt.show()
 # plt.imshow(res2, cmap="gray")
-label_image = image_get_label(img, mode="HIS_MODE")
+label_image = image_get_label(img, mode="EDGE_BASED")
 res2 = image_color_fill(label_image)
 plt.imsave("res2.jpg", res2)
 print(res2.shape)
